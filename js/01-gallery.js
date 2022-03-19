17c79b65fd11598d1f9c6dc9befbaa82cf0b448e
Alex <af88_88@mail.ru>
2022-03-19 17:40:10 +0300

    Основные файлы

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+
+
+console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery")
+
+function addGalleryMarkup(gallery) {
+    return gallery.map(({original, preview, description}) => 
+        `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image lazyload"
+                data-src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+        </div>
+        `
+    ).join('')
+}
+
+galleryEl.innerHTML = addGalleryMarkup(galleryItems)
+galleryEl.addEventListener('click', onImgClikc)
+
+function onImgClikc(e) {
+    e.preventDefault()
+    
+    if (e.target.nodeName !=="IMG") {
+        return
+    }
+    
+     backdrop(e)    
+}
+
+
+function backdrop(e) {
+      const instance = basicLightbox.create(`<img src="${e.target.dataset.source}">`);
+    instance.show();
+
+    window.addEventListener('keydown', onExitBackdrop);
+
+}
+
+function onExitBackdrop(e) {
+    if (e.code!=='Escape') {
+        return
+    }
+    document.querySelector('.basicLightbox').remove();
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery")
+
+function addGalleryMarkup(gallery) {
+    return gallery.map(({original, preview, description}) => 
+        `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"/>
+            </a>
+        </li>
+        `
+    ).join('')
+}
+
+
+
+galleryEl.innerHTML = addGalleryMarkup(galleryItems)
+
+const instance = new SimpleLightbox('.gallery a', {
+        
+            animationSpeed: 250,
+            loop: true,
+            enableKeyboard: true,
+            preloading: true,
+        docClose: true,
+        captionsData: 'alt',
+        }
+    );
